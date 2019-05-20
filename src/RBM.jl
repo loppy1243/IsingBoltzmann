9236b659d3176bf433b197a3838eebf521951679
Nicholas Todoroff <loppy1243@gmail.com>
2019-05-20 17:24:30 -0400

    grad-kernel: Kernelize kldiv_grad!
    Allows different kernel objects to be created which hold necessary state
    and can be called to perform the gradient in a certain way, e.g. on CPU
    or on GPU.

--- src/RBM.jl
+++ src/RBM.jl
@@ -7,8 +7,12 @@ using ..IsingBoltzmann: bitstrings, @default_first_arg
 using LinearAlgebra: mul!
 ## External ####################################################################
 using Random: GLOBAL_RNG
+using Reexport: @reexport
 
-export RestrictedBoltzmann, energy, partitionfunc, pdf, input_pdf, update!, train!, kldiv
+export RestrictedBoltzmann,
+       biastype, weightstype,
+       energy, partitionfunc, pdf, input_pdf, kldiv,
+       KLDivGradKernels, update!, train!
 
 struct RestrictedBoltzmann{
         T, V<:AbstractVector{T}, M<:AbstractMatrix{T}, Rf<:Ref{Union{T, Nothing}}
@@ -73,7 +77,13 @@ function RestrictedBoltzmann(
     )
 end
 
-Base.eltype(rbm::Type{<:RestrictedBoltzmann{T}}) where T = T
+Base.eltype(::Type{<:RestrictedBoltzmann{T}}) where T = T
+
+biastype(::Type{<:RestrictedBoltzmann{<:Any, V}}) where V = V
+biastype(rbm::RestrictedBoltzmann) = biastype(typeof(rbm))
+
+weightstype(::Type{<:RestrictedBoltzmann{<:Any, <:Any, M}}) where M = M
+weightstype(rbm::RestrictedBoltzmann) = weightstype(typeof(rbm))
 
 energy(rbm, inputs, hiddens) =
     -sum(rbm.inputbias.*inputs) - sum(hiddens.*rbm.hiddenbias) - sum(hiddens.*rbm.weights*inputs)
@@ -93,17 +103,14 @@ _partitionfunc(rbm) = rbm.partitionfunc[] = sum(
 pdf(rbm) = (inputs, hiddens) -> RBM.pdf(rbm, inputs, hiddens)
 pdf(rbm, inputs, hiddens) = exp(-energy(rbm, inputs, hiddens))/partitionfunc(rbm)
 input_pdf(rbm) = inputs -> RBM.input_pdf(rbm, inputs)
-input_pdf(rbm, inputs) = exp(-eff_energy(rbm, inputs))/partitionfunc(rbm)
+function input_pdf(rbm, inputs)
+    exp(-eff_energy(rbm, inputs))/partitionfunc(rbm)
+end
 
 sigmoid(x) = inv(one(x)+exp(-x))
 
 condprob_input1(rbm, k, h) = sigmoid(rbm.inputbias[k] + h'rbm.weights[:, k])
 condprob_hidden1(rbm, k, σ) = sigmoid(rbm.hiddenbias[k] + rbm.weights[k, :]'σ)
-
-function condavg_hidden!(rbm, σ)
-    mul!(rbm.condavg_hidden, rbm.weights, σ)
-    rbm.condavg_hidden .= sigmoid.(rbm.hiddenbias .+ rbm.condavg_hidden)
-end
 
 struct AltGibbsSampler{Rbm<:RestrictedBoltzmann} <: Random.Sampler{NTuple{2, BitVector}}
     rbm::Rbm
@@ -115,7 +122,7 @@ end
 AltGibbsSampler(init::UndefInitializer, rbm) =
     AltGibbsSampler(rbm, BitVector(init, rbm.inputsize), BitVector(init, rbm.hiddensize))
 @default_first_arg(
-function AltGibbsSampler(rng::AbstractRNG=GLOBAL_RNG, rbm, inputs0)
+function AltGibbsSampler(rng::AbstractRNG=GLOBAL_RNG, rbm::RestrictedBoltzmann, inputs0)
     hiddens = BitVector(undef, rbm.hiddensize)
 
     for k in eachindex(hiddens)
@@ -123,6 +130,16 @@ function AltGibbsSampler(rng::AbstractRNG=GLOBAL_RNG, rbm, inputs0)
     end
 
     AltGibbsSampler(rbm, copy(inputs0), hiddens)
+end)
+@default_first_arg(
+function AltGibbsSampler!(rng::AbstractRNG=GLOBAL_RNG, ag::AltGibbsSampler, inputs0)
+    ag.inputs .= inputs0
+
+    for k in eachindex(ag.hiddens)
+        ag.hiddens[k] = rand(rng) <= condprob_hidden1(ag.rbm, k, ag.inputs)
+    end
+
+    ag
 end)
 
 """
@@ -146,9 +163,14 @@ end
 Same as `altgibbs()`, but instead of constructing a new `AltGibbsSampler`, `ag`
 is modified in-place.
 """
+function altgibbs! end
 @default_first_arg function altgibbs!(rng=GLOBAL_RNG, ag::AltGibbsSampler, inputs0; copy=true)
     AltGibbsSampler!(rng, ag, inputs0)
     copy ? Base.copy(ag.hiddens) : ag.hiddens
+end
+@default_first_arg function altgibbs!(rng=GLOBAL_RNG, ag::AltGibbsSampler, inputs0)
+    AltGibbsSampler!(rng, ag, inputs0)
+    ag, copy(ag.hiddens)
 end
 
 function Random.rand(rng::AbstractRNG, cd::AltGibbsSampler; copy=true)
@@ -159,7 +181,7 @@ function Random.rand(rng::AbstractRNG, cd::AltGibbsSampler; copy=true)
         cd.hiddens[k] = rand(rng) <= condprob_hidden1(cd.rbm, k, cd.inputs)
     end
 
-    copy ? (Base.copy(cd.hiddens), Base.copy(cd.inputs)) : (cd.hiddens, cd.inputs)
+    copy ? (Base.copy(cd.inputs), Base.copy(cd.hiddens)) : (cd.inputs, cd.hiddens)
 end
 function Random.rand!(rng::AbstractRNG, (inputs, hiddens), cd::AltGibbsSampler)
     for k in eachindex(cd.inputs)
@@ -201,66 +223,157 @@ function kldiv(rbm, batch)
     -avg_log_likelihood - entrop
 end
 
-## There are two potential ways this should maybe be implemented. See doc/kldiv_grad.tex for
-## details.
-@default_first_arg function kldiv_grad!(rng=GLOBAL_RNG, rbm, batch)
-    z = zero(eltype(rbm))
-    rbm.inputgrad   .= z
-    rbm.hiddengrad  .= z
-    rbm.weightsgrad .= z
+module KLDivGradKernels
+    using ..RBM
+    using ..RBM: AltGibbsSampler
+    export KLDivGradKernel, ExactKernel, ApproxKernel, CuArrayKernel
 
-    L = length(batch)
-    for σ in batch
-        ## Method 1
-        σh_sampler = AltGibbsSampler(rng, rbm, σ)
-        h2, σ2 = rand(rng, σh_sampler; copy=false)
+    ## Subtypes must implement the method
+    ##     (::KLDivGradKernel)(rng, rbm)
+    abstract type KLDivGradKernel end
 
-        condavg_hidden!(rbm, σ2)
-        rbm.inputgrad   .+= σ2
-        rbm.hiddengrad  .+= rbm.condavg_hidden
-        mul!(rbm.hiddeninput_prod, h2, σ2')
-        rbm.weightsgrad .+= rbm.hiddeninput_prod
-
-        condavg_hidden!(rbm, σ)
-        rbm.inputgrad   .-= σ
-        rbm.hiddengrad  .-= rbm.condavg_hidden
-        mul!(rbm.hiddeninput_prod, rbm.condavg_hidden, σ')
-        rbm.weightsgrad .-= rbm.hiddeninput_prod
-
-#        ## Method 2
-#        σh_sampler, h = altgibbs(rbm, σ)
-#
-#        h2, σ2 = rand(σh_sampler; copy=false)
-#        rbm.inputgrad   .+= σ2 .- σ
-#        rbm.hiddengrad  .+= h2 .- h
-#        mul!(rbm.hiddeninput_prod, h2, σ2')
-#        rbm.weightsgrad .+= rbm.hiddeninput_prod
-#        mul!(rbm.hiddeninput_prd, h, σ')
-#        rbm.weightsgrad .-= rbm.hiddeninput_prod
+    Base.@kwdef struct PosNegNodes
+        pos_inputs::Vector{BitVector}
+        pos_hiddens::Vector{BitVector}
+        neg_inputs::Vector{BitVector}
+        neg_hiddens::Vector{BitVector}
     end
-    rbm.inputgrad   ./= L
-    rbm.hiddengrad  ./= L
-    rbm.weightsgrad ./= L
+    function PosNegNodes(rbm, batchsize)
+        PosNegNodes(
+            [BitVector(undef, rbm.inputsize)  for _ = 1:batchsize],
+            [BitVector(undef, rbm.hiddensize) for _ = 1:batchsize],
+            [BitVector(undef, rbm.inputsize)  for _ = 1:batchsize],
+            [BitVector(undef, rbm.hiddensize) for _ = 1:batchsize]
+        )
+    end
 
-    rbm.inputgrad, rbm.hiddengrad, rbm.weightsgrad
+    Base.@kwdef struct Grad{V, M}
+        inputs::V
+        hiddens::V
+        weights::M
+    end
+    Grad(rbm) = Grad{biastype(rbm), weightstype(rbm)}(
+        biastype(rbm)(undef, rbm.inputsize), biastype(rbm)(undef, rbm.hiddensize),
+        weightstype(rbm)(undef, rbm.hiddensize, rbm.inputsize)
+    )
+
+    ## See doc/kldiv_grad.tex for the difference between ExactKernel and ApproxKernel.
+    struct ExactKernel{
+            T, V<:AbstractVector{T}, M<:AbstractMatrix{T}, Rbm<:RestrictedBoltzmann{T, V, M}
+    } <: KLDivGradKernel
+        σh_sampler::AltGibbsSampler{Rbm}
+        grad::Grad{V, M}
+        pos_h::BitVector
+        condavg_h::Vector{Float64}
+        hσ_prod::Matrix{Float64}
+    end
+    ExactKernel(rbm) = ExactKernel{eltype(rbm), biastype(rbm), weightstype(rbm), typeof(rbm)}(
+        AltGibbsSampler(undef, rbm),
+        Grad(rbm),
+        BitVector(undef, rbm.hiddensize),
+        Vector{Float64}(undef, rbm.hiddensize),
+        Matrix{Float64}(undef, rbm.hiddensize, rbm.inputsize)
+    )
+
+    struct ApproxKernel{
+            T, V<:AbstractVector{T}, M<:AbstractMatrix{T}, Rbm<:RestrictedBoltzmann{T, V, M}
+    } <: KLDivGradKernel
+        σh_sampler::AltGibbsSampler{Rbm}
+        grad::Grad{V, M}
+        pos_h::BitVector
+        hσ_prod::Matrix{Float64}
+    end
+    ApproxKernel(rbm) = ApproxKernel{eltype(rbm), biastype(rbm), weightstype(rbm), typeof(rbm)}(
+        AltGibbsSampler(undef, rbm),
+        Grad(rbm),
+        BitVector(undef, rbm.hiddensize),
+        Matrix{Float64}(undef, rbm.hiddensize, rbm.inputsize)
+    )
+
+    struct CuArrayKernel <: KLDivGradKernel
+    end
+end
+@reexport using .KLDivGradKernels
+
+condavg_hidden!(out, rbm, σ) = out .= sigmoid.(rbm.hiddenbias .+ mul!(out, rbm.weights, σ))
+## Exact hidden conditional mean.
+function (kern::ExactKernel)(rng, rbm, batch)
+    z = zero(eltype(rbm))
+    fill!(kern.grad.inputs, z)
+    fill!(kern.grad.hiddens, z)
+    fill!(kern.grad.weights, z)
+
+    for σ⁺ in batch
+        ## NOTE the order of operations on kern.condavg_h in particular. As it stands, this is
+        ## the correct order
+        
+        altgibbs!(rng, kern.σh_sampler, σ⁺; copy=false)
+        σ⁻, h⁻ = rand(rng, kern.σh_sampler; copy=false)
+
+        kern.grad.inputs .+= σ⁻ .- σ⁺
+
+        kern.grad.hiddens .+= condavg_hidden!(kern.condavg_h, rbm, σ⁻)
+        kern.grad.hiddens .-= condavg_hidden!(kern.condavg_h, rbm, σ⁺)
+
+        kern.grad.weights .+= mul!(kern.hσ_prod, h⁻, σ⁻')
+        kern.grad.weights .-= mul!(kern.hσ_prod, kern.condavg_h, σ⁺')
+    end
+    L = length(batch)
+    kern.grad.inputs  ./= L
+    kern.grad.hiddens ./= L
+    kern.grad.weights ./= L
+
+    kern.grad.inputs, kern.grad.hiddens, kern.grad.weights
 end
 
-@default_first_arg function update!(rng=GLOBAL_RNG, rbm, batch)
-    kldiv_grad!(rbm, batch)
+## Approximate hidden conditional mean.
+function (kern::ApproxKernel)(rng, rbm, batch)
+    z = zero(eltype(rbm))
+    fill!(kern.grad.inputs, z)
+    fill!(kern.grad.hiddens, z)
+    fill!(kern.grad.weights, z)
 
-    rbm.inputbias  .-= rbm.learning_rate.*rbm.inputgrad
-    rbm.hiddenbias .-= rbm.learning_rate.*rbm.hiddengrad
-    rbm.weights    .-= rbm.learning_rate.*rbm.weightsgrad
+    for σ⁺ in batch
+        kern.pos_h .= altgibbs!(rng, kern.σh_sampler, σ; copy=false)
+        σ⁻, h⁻ = rand(rng, kern.σh_sampler; copy=false)
+
+        kern.grad.inputs .+= σ⁻ .- σ
+        kern.grad.hiddens .+= h⁻ .- h
+        kern.grad.weights .+= mul!(kern.hσ_prod, h⁻, σ⁻')
+        kern.grad.weights .-= mul!(kern.hσ_prod, h⁺, σ⁺')
+    end
+    L = length(batch)
+    kern.grad.inputs  ./= L
+    kern.grad.hiddens ./= L
+    kern.grad.weights ./= L
+
+    kern.grad.inputs, kern.grad.hiddens, kern.grad.weights
+end
+
+@default_first_arg update!(rng::AbstractRNG=GLOBAL_RNG, rbm, batch) =
+    update!(rng, rbm, ExactKernel(rbm), batch)
+@default_first_arg function update!(
+        rng::AbstractRNG=GLOBAL_RNG, rbm, kern::KLDivGradKernel, batch
+)
+    σgrad, hgrad, Wgrad = kern(rng, rbm, batch)
+
+    rbm.inputbias  .-= rbm.learning_rate.*σgrad
+    rbm.hiddenbias .-= rbm.learning_rate.*hgrad
+    rbm.weights    .-= rbm.learning_rate.*Wgrad
     rbm.partitionfunc[] = nothing
 
     rbm
 end
 
-@default_first_arg function train!(rng=GLOBAL_RNG, rbm, minibatches)
+@default_first_arg train!(rng::AbstractRNG=GLOBAL_RNG, rbm, minibatches) =
+    train!(rng, rbm, ExactKernel(rbm), minibatches)
+@default_first_arg function train!(
+        rng::AbstractRNG=GLOBAL_RNG, rbm, kern::KLDivGradKernel, minibatches
+)
     perm = randperm(rng, size(minibatches, ndims(minibatches)))
     permute!(minibatches, perm)
 
-    for (k, b) in enumerate(minibatches)
+    for b in minibatches
         update!(rng, rbm, b)
     end
 
