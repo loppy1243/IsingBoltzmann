19899e4b1b12b4b34aa92ad8236159af3826c83d
Nicholas Todoroff <loppy1243@gmail.com>
2019-06-07 17:14:27 -0400

    training: Update `run/*` to accord with training changes

--- run/1D.jl
+++ run/1D.jl
@@ -4,113 +4,176 @@ using IsingBoltzmann
 ## stdlib ######################################################################
 import Dates
 ## External ####################################################################
-using Plots; Plots.gr()
+import Flux.Optimise
+using Plots
 ################################################################################
 ## Individual: Internal ########################################################
-using IsingBoltzmann: bitstrings
+using IsingBoltzmann: bitstrings, batch
 ## stdlib ######################################################################
-using Random: MersenneTwister
+using Random: MersenneTwister, shuffle!
 using Printf: @sprintf
+using Statistics: mean
 
-const CONFIG = IsingBoltzmann.AppConfig(
+DEFAULT_CONFIG() = IsingBoltzmann.AppConfig(
     spinsize=(6,),
     coupling=1.0,
     invtemp=0.4,
     boundarycond=Ising.FixedBoundary(),
     
     nhiddens=6,
-    learning_rate=0.1,
     kldiv_grad_kernel=KLDivGradKernels.ExactKernel,
     kernel_kwargs=Dict(:cd_num=>5),
+    optimizer=Optimise.Descent(0.01),
 
-    nepochs=1000,
-    nsamples=10^5,
-    batchsize=50,
     rng=MersenneTwister(1022150136457226227)
 )
 
-function main(; sample_epochs=[10, 500, 1000], debug=false)
+Base.@kwdef mutable struct LocalConfig
+    nsamples::Int=10^5
+    minibatchsize::Int=50
+    nminibatches::Int=div(nsamples, minibatchsize)
+    nepochs::Int=1000
+
+    kldiv_samples=(epochs=1:nepochs,    minibatches=nminibatches)
+    pdf_samples  =(epochs=(10, 30, 50), minibatches=nminibatches)
+
+    n_Δkldiv_avg_samples::Int=20
+    Δkldiv_avg_atol::Float64=1e-2
+
+    kldiv_plotfile::String="kldiv_1D.pdf"
+    pdf_plotfile::String="pdf_1D.pdf"
+
+    debug::Bool=false
+    plot::Bool=true
+end
+
+function __init__()
+    Plots.gr()
+    nothing
+end
+
+tuplecomp((a1, b1), (a2, b2)) = a1 < a2 || a1 == a2 && b1 <= b2
+
+main(; kwargs...) = main(LocalConfig(; kwargs...), DEFAULT_CONFIG())
+function main(LCONFIG, CONFIG)
     isingmodel = ising(CONFIG)
-
-    kldivs_exact = Vector{Float64}(undef, CONFIG.nepochs+1)
-    kldivs_approx = Vector{Float64}(undef, CONFIG.nepochs+1)
-
     ising_pf = Ising.partitionfunc(isingmodel)
+    ising_sampler = MetropolisIsingSampler(isingmodel; init=dims->spinrand(CONFIG.rng, dims))
+    ising_samples = rand(CONFIG.rng, ising_sampler, LCONFIG.nsamples)
 
-    debug && print("Computing Ising PDF...")
-    prob_exact = Ising.pdf.(Ref(isingmodel), spinstates(isingmodel); pfunc=ising_pf)
-    debug && println(" Done.")
-    prob_rbm = Dict(epoch => Vector{Float64}(undef, 2^CONFIG.nspins)
-        for epoch in sample_epochs
-    )
+    prob_rbm = Dict{NTuple{2, Int}, Vector{Float64}}()
+    kldivs_exact = Dict{NTuple{2, Int}, Float64}()
+    kldivs_approx = Dict{NTuple{2, Int}, Float64}()
 
     cb = callback(
-        debug, ising_pf, sample_epochs, prob_exact, prob_rbm, kldivs_exact, kldivs_approx
+        isingmodel, ising_pf, prob_rbm, kldivs_exact, kldivs_approx, LCONFIG, CONFIG
     )
-    debug && println("Creating and training RBM:")
-    train(cb, isingmodel, CONFIG)
 
-    debug && print("Creating kldiv plot...")
-    plot(0:CONFIG.nepochs, [kldivs_exact kldivs_approx], label=["Exact" "Approx"],
-        yscale = :log10,
+    LCONFIG.debug && println("Creating and training RBM:")
+    ising_minibatches = batch(ising_samples, LCONFIG.minibatchsize)
+    train(cb, LCONFIG.nepochs, ()->shuffle!(CONFIG.rng, ising_minibatches), CONFIG)
+
+    LCONFIG.debug && print("Creating kldiv plot...")
+
+    kldivs_points = sort(collect(keys(kldivs_exact)); lt=tuplecomp)
+    kldivs_exact_vals = [kldivs_exact[key] for key in kldivs_points]
+    kldivs_approx_vals = [kldivs_approx[key] for key in kldivs_points]
+
+    format((a, b)) = string(b, "\n", a)
+    plot(format.(kldivs_points), [kldivs_exact_vals kldivs_approx_vals],
+        label=["Exact" "Approx"],
+        yscale=:log10,
         title="Ising 1D RBM KL Divergence", xlabel="Epoch", ylabel="KL Divergence"
     )
-    annotate!([(0, 0, text(Dates.now(), "monospace", 12))])
-    savefig("kldiv_1D.pdf")
-    debug && println(" Done.")
+    annotate!([(15, 0, text(Dates.now(), "monospace", 12))])
+    savefig(LCONFIG.kldiv_plotfile)
+    LCONFIG.debug && println(" Done.")
 
-    debug && print("Creating PDF plot...")
+    LCONFIG.plot || return nothing
+
+    LCONFIG.debug && print("Creating PDF plot...")
+    prob_exact = Ising.pdf.(Ref(isingmodel), spinstates(isingmodel); pfunc=ising_pf)
     first = true
     plts = []
-    for epoch in sample_epochs
+    for key in sort(collect(keys(prob_rbm)); lt=tuplecomp)
         plt = plot(1:2^CONFIG.nspins, prob_exact, label="Exact",
             yscale = :log10, markershape=:auto,
-            title="Epochs=$epoch", xlabel="\$\\sigma\$", ylabel="\$P(\\sigma)\$",
+            title="(Epochs, Samples)=$key", xlabel="\$\\sigma\$", ylabel="\$P(\\sigma)\$",
             legend=(first && (:top))
         )
-        plot!(1:2^CONFIG.nspins, prob_rbm[epoch], label="RBM", markershape=:auto)
+        plot!(1:2^CONFIG.nspins, prob_rbm[key], label="RBM", markershape=:auto)
 
         push!(plts, plt)
         first = false
     end
     sz = Plots.default(:size)
     plot(plts...,
-         layout=(length(sample_epochs), 1),
-         size=(sz[1], length(sample_epochs)*sz[2])
+         layout=(length(prob_rbm), 1),
+         size=(sz[1], length(prob_rbm)*sz[2])
     )
     annotate!([(0, 0, text(Dates.now(), "monospace", 12))])
-    savefig("pdf_1D.pdf")
-    debug && println(" Done.")
+    savefig(LCONFIG.pdf_plotfile)
+    LCONFIG.debug && println(" Done.")
+
+    nothing
 end
 
-callback(debug, ising_pf, sample_epochs, prob_exact, prob_rbm, kldivs_exact, kldivs_approx) =
-function(epoch, nepochs, rbm, ising, ising_samples)
-    epochfmt(epoch) = lpad(epoch, ndigits(nepochs))
+function callback(
+        ising, ising_pf, prob_rbm, kldivs_exact, kldivs_approx, LCONFIG, CONFIG
+)
+    kld_exact_prev = nothing
+    kld_approx_prev = nothing
+
+    full = false
+    Δidx = 1
+    Δklds = Vector{Float64}(undef, LCONFIG.n_Δkldiv_avg_samples)
+function(epoch, minibatchnum, rbm, minibatch)
+    epochfmt(epoch) = lpad(epoch, ndigits(LCONFIG.nepochs))
     numfmt(num) = @sprintf("%.5f", num)
     deltafmt(num) = @sprintf("%+.5f", num)
 
-    rbm_pf = RBM.partitionfunc(rbm)
+    ret = false
 
-    debug && print("    Computing kldiv_exact...")
-    kld_exact = kldiv(rbm, Ising.pdf(ising; pfunc=ising_pf); pfunc=rbm_pf)
-    debug && print(" Done.\n    Computing kldiv_approx...")
-    kld_approx = kldiv(rbm, ising_samples; pfunc=rbm_pf)
-    debug && println(" Done.")
-    kldivs_exact[epoch+1] = kld_exact
-    kldivs_approx[epoch+1] = kld_approx
-    Δexact = kld_exact - kldivs_exact[max(1, epoch)]
-    Δapprox = kld_approx - kldivs_approx[max(1, epoch)]
-    println(
-        epochfmt(epoch), ": ",
-        "KL_approx=$(numfmt(kld_approx)), KL_exact=$(numfmt(kld_exact)), ",
-        "ΔKL_approx=$(deltafmt(Δapprox)), ΔKL_exact=$(deltafmt(Δexact))"
-    )
+    idx = (epoch, minibatchnum)
 
-    if epoch in sample_epochs
-        debug && print("    Calculating RBM PDF for epoch ", epoch, "...")
-        prob_rbm[epoch] .= RBM.input_pdf.(Ref(rbm), bitstrings(rbm.inputsize); pfunc=rbm_pf)
-        debug && println(" Done.")
+    if all(in.(idx, Tuple(LCONFIG.kldiv_samples)))
+        rbm_pf = RBM.partitionfunc(rbm)
+
+        LCONFIG.debug && print("    Computing kldiv_exact...")
+        kld_exact = kldiv(rbm, Ising.pdf(ising; pfunc=ising_pf); pfunc=rbm_pf)
+        LCONFIG.debug && println(" Done.")
+
+        LCONFIG.debug && print("    Computing kldiv_approx...")
+        kld_approx = kldiv(rbm, minibatch; pfunc=rbm_pf)
+        LCONFIG.debug && println(" Done.")
+
+        Δexact  = isnothing(kld_exact_prev)  ? 0.0 : kld_exact - kld_exact_prev
+        Δapprox = isnothing(kld_approx_prev) ? 0.0 : kld_approx - kld_approx_prev
+        println(
+            epochfmt(epoch), ": ",
+            "KL_approx=$(numfmt(kld_approx)), KL_exact=$(numfmt(kld_exact)), ",
+            "ΔKL_approx=$(deltafmt(Δapprox)), ΔKL_exact=$(deltafmt(Δexact))"
+        )
+
+        kld_exact_prev = kld_exact; kld_approx_prev = kld_approx
+        kldivs_exact[idx] = kld_exact; kldivs_approx[idx] = kld_approx
+
+        Δklds[Δidx] = kld_exact
+
+        Δidx == LCONFIG.n_Δkldiv_avg_samples && (full = true)
+        Δidx = Δidx % LCONFIG.n_Δkldiv_avg_samples + 1
+        full && (ret = ret || isapprox(mean(Δklds), 0.0; atol=LCONFIG.Δkldiv_avg_atol))
     end
-end
+
+    if all(in.(idx, Tuple(LCONFIG.pdf_samples)))
+        rbm_pf = RBM.partitionfunc(rbm)
+
+        LCONFIG.debug && print("    Calculating RBM PDF for epoch ", epoch, "...")
+        prob_rbm[idx] = RBM.input_pdf.(Ref(rbm), bitstrings(rbm.inputsize); pfunc=rbm_pf)
+        LCONFIG.debug && println(" Done.")
+    end
+
+    ret
+end end
 
 end # module IsingBoltzmann1DRun
